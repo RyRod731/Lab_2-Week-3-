76cd46a8c3eeaa84f01eb66546f0ff76acbaae33
CBowneUCF <156275809+CBowneUCF@users.noreply.github.com>
2024-09-04 14:49:28 -0400

    Cube Complete

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -1,23 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Cube : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float size = 1f;
 }
 
 #if UNITY_EDITOR
@@ -25,7 +19,49 @@ public class Cube : MonoBehaviour
 [CustomEditor(typeof(Cube)), CanEditMultipleObjects]
 public class Cube_Editor : Editor
 {
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
 
+        var size = serializedObject.FindProperty("size");
+
+        if (size.floatValue < 1f)
+            EditorGUILayout.HelpBox("The cubes' sizes cannot be smaller than 1", MessageType.Warning);
+        if (size.floatValue > 2f)
+            EditorGUILayout.HelpBox("The cubes' sizes cannot be bigger than 2", MessageType.Warning);
+        
+        EditorGUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Select All Cubes"))
+        {
+            var allCubeBehaviors = FindObjectsOfType<Cube>();
+            var allCubeGOs = allCubeBehaviors
+                .Select(enemy => enemy.gameObject)
+                .ToArray();
+            Selection.objects = allCubeGOs;
+        }
+
+        if (GUILayout.Button("Clear Selection")) Selection.objects = null;
+
+        EditorGUILayout.EndHorizontal();
+
+        bool SelectedIsEnabled = Selection.activeGameObject != null && Selection.activeGameObject.activeSelf;
+
+        var defaultColor = GUI.backgroundColor;
+        GUI.backgroundColor = SelectedIsEnabled ? Color.red : Color.green;
+
+        if (GUILayout.Button("Disable/Enable all enemy", GUILayout.Height(40)))
+        {
+            foreach (var enemy in GameObject.FindObjectsOfType<Cube>(true))
+            {
+                Undo.RecordObject(enemy.gameObject, "Disable/Enable enemy");
+                enemy.gameObject.SetActive(!SelectedIsEnabled);
+            }
+            
+        }
+
+        GUI.backgroundColor = defaultColor;
+    }
 }
 
 #endif
